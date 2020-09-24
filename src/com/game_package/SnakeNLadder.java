ab802b6d2ab8e190b54113fbfc7175257f714f9e
avnish986 <avnishgupta986@gmail.com>
2020-09-24 15:21:01 +0530

    [Avnish]Added:UC3

--- src/com/game_package/SnakeNLadder.java
+++ src/com/game_package/SnakeNLadder.java
@@ -2,11 +2,27 @@ package com.game_package;
 
 public class SnakeNLadder {
 	public static final int player_count=1;
-	public static final int position=0;
+	public static final int noPlay=0;
+	public static final int ladder=1;
+	public static final int snake=2;
 	public static void main(String[] args) {
 		System.out.println("Welcome to game");
+		int position=0;
 		int valDice=(int)(Math.floor(Math.random()*10)%6)+1;
 		System.out.println("player rolled no. "+valDice);
+		
+		int option=(int)(Math.floor(Math.random()*10)%3);
+		
+		if(option == noPlay)
+			System.out.println("Player stays at "+position);
+		else if(option == ladder) {
+			position+=valDice;
+			System.out.println("Player moves ahead at "+position);
+		}
+		else {
+			position-=valDice;
+			System.out.println("Player moves back at "+position);
+		}
 		
 	}
 
