ef0d60d4deead42d6638545665c8d5de9cc0abb4
avnish986 <avnishgupta986@gmail.com>
2020-09-24 15:04:41 +0530

    [Avnish]Added:UC2

--- src/com/game_package/SnakeNLadder.java
+++ src/com/game_package/SnakeNLadder.java
@@ -4,7 +4,9 @@ public class SnakeNLadder {
 	public static final int player_count=1;
 	public static final int position=0;
 	public static void main(String[] args) {
-		System.out.println("Player at : "+position);
+		System.out.println("Welcome to game");
+		int valDice=(int)(Math.floor(Math.random()*10)%6)+1;
+		System.out.println("player rolled no. "+valDice);
 		
 	}
 
